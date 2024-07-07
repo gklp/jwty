c1f5917570c3b0e76a2268bd10ef846a0936060f
GitHub Action <action@github.com>
2024-07-07 11:16:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/gklp/jwty/MyBundle.kt
+++ src/main/kotlin/com/github/gklp/jwty/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gklp.jwty
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gklp/jwty/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gklp/jwty/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gklp.jwty.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
